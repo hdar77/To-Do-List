5845b5158053a9f87103a5edcb9e12e4ffd020d0
Banause1508 <168537529+Banause1508@users.noreply.github.com>
2024-07-05 13:22:44 +0200

    Update TasksManager.java

--- src/main/java/org/ase/TasksManager.java
+++ src/main/java/org/ase/TasksManager.java
@@ -1,4 +1,41 @@
-package org.ase;
+import java.util.ArrayList;
 
-/** class for tasks manager. */
-public class TasksManager {}
+public class TaskManager {
+    private ArrayList<Task> tasks;
+
+    public TaskManager() {
+        tasks = new ArrayList<>();
+    }
+
+    public void addTask(Task task) {
+        tasks.add(task);
+        System.out.println("Aufgabe hinzugefügt: " + task.getDescription());
+    }
+
+    public void removeTask(int index) {
+        if (index >= 0 && index < tasks.size()) {
+            System.out.println("Aufgabe entfernt: " + tasks.remove(index).getDescription());
+        } else {
+            System.out.println("Ungültiger Index.");
+        }
+    }
+
+    public void showTasks() {
+        if (tasks.isEmpty()) {
+            System.out.println("Keine Aufgaben in der Liste.");
+        } else {
+            System.out.println("Aktuelle Aufgaben:");
+            for (int i = 0; i < tasks.size(); i++) {
+                System.out.println((i + 1) + ". " + tasks.get(i).getDescription());
+            }
+        }
+    }
+
+    public ArrayList<Task> getTasks() {
+        return tasks;
+    }
+
+    public void setTasks(ArrayList<Task> tasks) {
+        this.tasks = tasks;
+    }
+}
