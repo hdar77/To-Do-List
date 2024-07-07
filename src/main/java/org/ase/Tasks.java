9a99e30c95bb664faafe0e3a2fed1b0bec71048d
Banause1508 <168537529+Banause1508@users.noreply.github.com>
2024-07-07 19:20:12 +0200

    Update Tasks.java

--- src/main/java/org/ase/Tasks.java
+++ src/main/java/org/ase/Tasks.java
@@ -1,16 +1,33 @@
-public class Task {
+package org.ase;
+
+public class Tasks {
+    private String title;
     private String description;
 
-    public Task(String description) {
+    public Tasks(String title, String description) {
+        this.title = title;
         this.description = description;
+    }
+
+    // Getter und Setter
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
         return description;
     }
 
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
     @Override
     public String toString() {
-        return description;
+        return "Tasks [title=" + title + ", description=" + description + "]";
     }
 }
